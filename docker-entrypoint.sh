5c403f685362d6de5bbde31539132d9f2fb8a238
aly-saleh <aly.saleh@gmail.com>
2016-02-03 17:56:41 +0200

    Update docker-entrypoint.sh
    update cataline base/home directory

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,6 +14,7 @@ if [ -z "$POSTGRES_PASSWORD" ]; then
     echo "POSTGRES_PASSWORD environment variable required"
     exit 1
 fi
+CATALINE_BASE = "/var/lib/tomcat7"
 
 generatedb() {
     echo "GENERATING DATABASE..."
@@ -29,38 +30,38 @@ generatedb() {
 }
 
 extractdmswar() {
-    cp ${MCC_DIR}/buildoutput/DMS.war /usr/local/apache-tomcat/webapps/
-    mkdir /usr/local/apache-tomcat/webapps/DMS
-    unzip -o /usr/local/apache-tomcat/webapps/DMS.war -d /usr/local/apache-tomcat/webapps/DMS
+    cp ${MCC_DIR}/buildoutput/DMS.war $CATALINE_BASE/webapps/
+    mkdir $CATALINE_BASE/webapps/DMS
+    unzip -o $CATALINE_BASE/webapps/DMS.war -d $CATALINE_BASE/webapps/DMS
 }
 
 patchdbproperties() {
     echo "PATCHING DB PROPERTIES..."
 
     # DMS/WEB-INF/classes/CMEngine.properties
-    sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/CMEngine.properties
-    sed -i "s#DB_USERNAME=.*#DB_USERNAME=${POSTGRES_USERNAME}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/CMEngine.properties
-    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/CMEngine.properties
+    sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/CMEngine.properties
+    sed -i "s#DB_USERNAME=.*#DB_USERNAME=${POSTGRES_USERNAME}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/CMEngine.properties
+    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/CMEngine.properties
 
     # DMS/WEB-INF/classes/com/trilogy/fs/dms/DMSBackbone.properties
-    sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/DMSBackbone.properties
-    sed -i "s#DB_USERNAME=.*#DB_USERNAME=${POSTGRES_USERNAME}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/DMSBackbone.properties
-    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/DMSBackbone.properties
+    sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/DMSBackbone.properties
+    sed -i "s#DB_USERNAME=.*#DB_USERNAME=${POSTGRES_USERNAME}#g"$CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/DMSBackbone.properties
+    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/DMSBackbone.properties
 
     # DMS/WEB-INF/classes/com/trilogy/fs/dms/core/lock/DMSDistributedLockService.properties
-    sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/core/lock/DMSDistributedLockService.properties
-    sed -i "s#DB_USERNAME=.*#DB_USERNAME=${POSTGRES_USERNAME}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/core/lock/DMSDistributedLockService.properties
-    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/core/lock/DMSDistributedLockService.properties
+    sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/core/lock/DMSDistributedLockService.properties
+    sed -i "s#DB_USERNAME=.*#DB_USERNAME=${POSTGRES_USERNAME}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/core/lock/DMSDistributedLockService.properties
+    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/core/lock/DMSDistributedLockService.properties
 
     # DMS/WEB-INF/classes/ivizgroup.properties
-    sed -i "s#db.url=.*#db.url=jdbc:postgresql://${POSTGRES_CONNECTION}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/ivizgroup.properties
-    sed -i "s#db.user=.*#db.user=${POSTGRES_USERNAME}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/ivizgroup.properties
-    sed -i "s#db.password=.*#db.password=${POSTGRES_PASSWORD}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/ivizgroup.properties
+    sed -i "s#db.url=.*#db.url=jdbc:postgresql://${POSTGRES_CONNECTION}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/ivizgroup.properties
+    sed -i "s#db.user=.*#db.user=${POSTGRES_USERNAME}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/ivizgroup.properties
+    sed -i "s#db.password=.*#db.password=${POSTGRES_PASSWORD}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/ivizgroup.properties
 
     # DMS/WEB-INF/classes/ivizgroupLDAP.properties
-    sed -i "s#db.url=.*#db.url=jdbc:postgresql://${POSTGRES_CONNECTION}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/ivizgroupLDAP.properties
-    sed -i "s#db.user=.*#db.user=${POSTGRES_USERNAME}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/ivizgroupLDAP.properties
-    sed -i "s#db.password=.*#db.password=${POSTGRES_PASSWORD}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/ivizgroupLDAP.properties
+    sed -i "s#db.url=.*#db.url=jdbc:postgresql://${POSTGRES_CONNECTION}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/ivizgroupLDAP.properties
+    sed -i "s#db.user=.*#db.user=${POSTGRES_USERNAME}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/ivizgroupLDAP.properties
+    sed -i "s#db.password=.*#db.password=${POSTGRES_PASSWORD}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/ivizgroupLDAP.properties
 
     # DMS/WEB-INF/classes/local.properties
     sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/local.properties
@@ -68,45 +69,45 @@ patchdbproperties() {
     sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/local.properties
 
     # DMS/WEB-INF/classes/mcc.properties
-    sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/mcc.properties
-    sed -i "s#DB_USERNAME=.*#DB_USERNAME=${POSTGRES_USERNAME}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/mcc.properties
-    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/mcc.properties
+    sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/mcc.properties
+    sed -i "s#DB_USERNAME=.*#DB_USERNAME=${POSTGRES_USERNAME}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/mcc.properties
+    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/mcc.properties
 
     # DMS/WEB-INF/classes/com/trilogy/html/gui/Multi.properties
-    sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/html/gui/Multi.properties
-    sed -i "s#DB_USERNAME=.*#DB_USERNAME=${POSTGRES_USERNAME}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/html/gui/Multi.properties
-    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/html/gui/Multi.properties
+    sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/html/gui/Multi.properties
+    sed -i "s#DB_USERNAME=.*#DB_USERNAME=${POSTGRES_USERNAME}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/html/gui/Multi.properties
+    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/html/gui/Multi.properties
 
     # DMS/WEB-INF/classes/UserAcl2ServiceServerBackbone.properties
-    sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/UserAcl2ServiceServerBackbone.properties
-    sed -i "s#DB_USERNAME=.*#DB_USERNAME=${POSTGRES_USERNAME}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/UserAcl2ServiceServerBackbone.properties
-    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/UserAcl2ServiceServerBackbone.properties
+    sed -i "s#JDBC_URL=.*#JDBC_URL=jdbc:postgresql://${POSTGRES_CONNECTION}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/UserAcl2ServiceServerBackbone.properties
+    sed -i "s#DB_USERNAME=.*#DB_USERNAME=${POSTGRES_USERNAME}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/UserAcl2ServiceServerBackbone.properties
+    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=${POSTGRES_PASSWORD}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/UserAcl2ServiceServerBackbone.properties
 
     # DMS/WEB-INF/classes/com/versata/adhoc/resources/adhoc.properties
-    sed -i "s#jdbcUrl=.*#jdbcUrl=jdbc:postgresql://${POSTGRES_CONNECTION}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/versata/adhoc/resources/adhoc.properties
-    sed -i "s#jdbcUser=.*#jdbcUser=${POSTGRES_USERNAME}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/versata/adhoc/resources/adhoc.properties
-    sed -i "s#jdbcPassword=.*#jdbcPassword=${POSTGRES_PASSWORD}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/versata/adhoc/resources/adhoc.properties
+    sed -i "s#jdbcUrl=.*#jdbcUrl=jdbc:postgresql://${POSTGRES_CONNECTION}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/versata/adhoc/resources/adhoc.properties
+    sed -i "s#jdbcUser=.*#jdbcUser=${POSTGRES_USERNAME}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/versata/adhoc/resources/adhoc.properties
+    sed -i "s#jdbcPassword=.*#jdbcPassword=${POSTGRES_PASSWORD}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/versata/adhoc/resources/adhoc.properties
 
     # DMS/WEB-INF/classes/mcc.xml
-    sed -i "s#name=\"JDBC_URL\" value=\"[^\\""]*\"#name=\"JDBC_URL\" value=\"jdbc:postgresql://${POSTGRES_CONNECTION}\"#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/mcc.xml
-    sed -i "s#name=\"DB_USERNAME\" value=\"[^\\""]*\"#name=\"DB_USERNAME\" value=\"${POSTGRES_USERNAME}\"#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/mcc.xml
-    sed -i "s#name=\"DB_PASSWORD\" value=\"[^\\""]*\"#name=\"DB_PASSWORD\" value=\"${POSTGRES_PASSWORD}\"#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/mcc.xml
+    sed -i "s#name=\"JDBC_URL\" value=\"[^\\""]*\"#name=\"JDBC_URL\" value=\"jdbc:postgresql://${POSTGRES_CONNECTION}\"#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/mcc.xml
+    sed -i "s#name=\"DB_USERNAME\" value=\"[^\\""]*\"#name=\"DB_USERNAME\" value=\"${POSTGRES_USERNAME}\"#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/mcc.xml
+    sed -i "s#name=\"DB_PASSWORD\" value=\"[^\\""]*\"#name=\"DB_PASSWORD\" value=\"${POSTGRES_PASSWORD}\"#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/mcc.xml
 }
 
 patchnipr() {
     echo "PATCHING NIPR PROPERTIES..."
 
     # DMS/WEB-INF/classes/com/trilogy/fs/dms/niprgateway/GatewayIntegration.properties
-    sed -i "s#CustomerID=.*#CustomerID=${NIPR_USER}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/niprgateway/GatewayIntegration.properties
-    sed -i "s#Password=.*#Password=${NIPR_PASSWORD}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/niprgateway/GatewayIntegration.properties
+    sed -i "s#CustomerID=.*#CustomerID=${NIPR_USER}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/niprgateway/GatewayIntegration.properties
+    sed -i "s#Password=.*#Password=${NIPR_PASSWORD}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/niprgateway/GatewayIntegration.properties
 
     # DMS/WEB-INF/classes/com/trilogy/fs/dms/pdb/AccountInformation.properties
-    sed -i "s#CustomerID=.*#CustomerID=${NIPR_USER}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/pdb/AccountInformation.properties
-    sed -i "s#Password=.*#Password=${NIPR_PASSWORD}#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/pdb/AccountInformation.properties
+    sed -i "s#CustomerID=.*#CustomerID=${NIPR_USER}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/pdb/AccountInformation.properties
+    sed -i "s#Password=.*#Password=${NIPR_PASSWORD}#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/pdb/AccountInformation.properties
     
     # DMS/WEB-INF/classes/com/trilogy/fs/dms/pdb/DBIntegrationManager.properties
     if [ "$NIPR_BETA" == "true" -o "$NIPR_BETA" == "TRUE" ]; then
-        sed -i "s#https://pdb-services.nipr.com/#https://pdb-services-beta.nipr.com/#g" /usr/local/apache-tomcat/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/pdb/PDBIntegrationManager.properties
+        sed -i "s#https://pdb-services.nipr.com/#https://pdb-services-beta.nipr.com/#g" $CATALINE_BASE/webapps/DMS/WEB-INF/classes/com/trilogy/fs/dms/pdb/PDBIntegrationManager.properties
     fi
 }
 
