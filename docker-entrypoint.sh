1861d1da54bfba866a617ac7124abeb12d8e770a
aly-saleh <aly.saleh@gmail.com>
2016-02-21 20:04:01 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -39,8 +39,16 @@ deploywar() {
     cp ${MCC_DIR}/buildoutput/*.war $CATALINA_BASE/webapps/
 
     # DMS
+    rm -rf $CATALINA_BASE/webapps/DMS
     mkdir $CATALINA_BASE/webapps/DMS
     unzip -o $CATALINA_BASE/webapps/DMS.war -d $CATALINA_BASE/webapps/DMS
+    rm $CATALINA_BASE/webapps/DMS.war
+
+    # IPA
+    rm -rf $CATALINA_BASE/webapps/IPA
+    mkdir $CATALINA_BASE/webapps/IPA
+    unzip -o $CATALINA_BASE/webapps/IPA.war -d $CATALINA_BASE/webapps/IPA
+    rm $CATALINA_BASE/webapps/IPA.war
 }
 
 patchdbproperties() {
