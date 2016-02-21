ff33c04fe42aa0e5fd6c5cb13a131d65853c86fc
aly-saleh <aly.saleh@gmail.com>
2016-02-21 19:59:55 +0200

    Update docker-entrypoint.sh
    remove DMS dir and recreate it before deploying DMS war.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -35,12 +35,13 @@ generatedb() {
 
 deploywar() {
     echo "DEPLOYING WAR..."
-    
-    cp ${MCC_DIR}/buildoutput/*.war $CATALINA_BASE/webapps/
 
     # DMS
+    rm -rf $CATALINA_BASE/webapps/DMS
     mkdir $CATALINA_BASE/webapps/DMS
+    cp ${MCC_DIR}/buildoutput/*.war $CATALINA_BASE/webapps/
     unzip -o $CATALINA_BASE/webapps/DMS.war -d $CATALINA_BASE/webapps/DMS
+    rm $CATALINA_BASE/webapps/DMS.war
 }
 
 patchdbproperties() {
